f6296ca774111405e4db22c17518c7bb5d460cfb
imalirezapy <mousaviali1381@gmail.com>
2023-08-31 18:41:06 +0330

    -+improve code

--- pyramid-triangles.py
+++ pyramid-triangles.py
@@ -1,46 +1,18 @@
 # github: https://github.com/imalirezapy
 # Hope you enjoy (:
 
-x = int(input())
+n = int(input())
+t = 5 # triangle size
+m = lambda v: v*2*(t-1)+1
+w, w1 = m(n), m(1)-4
 
-n = 1 + (3 * x) + x
-bs = list(range(n-1, -1, -1)) # back space
-cs = [] # center space
-for i in range(x+1):
-    cs.extend([0, 1, 3, 5])
-
-ll = [] # last line stars
-for i in range(1, x+1):
-    n2 = 1 + (3 * i) + i
-    ll.append((n2 * 2) - 1)
-
-
-print(" " * bs[0] + "*")
-c = 1
-cl = 0
-cl2 = 0
-for i in range(1, n-1):
-    if cl2 == 0:
-        string = " " * bs[i]+"*" + " "*cs[c] + "*"
-        sl = list(string)
-        if cl > 0:
-    
-            if cs[c] == 1:
-                sl.append((("     *") + " "*cs[c] + "*")*cl)
-            elif cs[c] == 3:
-                sl.append((("   *") + " "*cs[c] + "*")*cl)
-            elif cs[c] == 5:
-                sl.append(((" *") + " "*cs[c] + "*")*cl)
-                
-        print("".join(sl))
-    elif cl > 0:
-        cl2 -= 1   
-    c += 1
-    if cs[c] == 0:
-        if ll[cl] == ll[0]:
-            print(" "*bs[i+1] +ll[cl]*"*", end="")
-        else:
-            print(" " * (bs[i+1])+ ll[cl] *"*", end="")
-        cl += 1
-        cl2 += 1
-        print()
+for i in range(n):
+    mi = m(i)
+    sp = (w-mi)//2
+    print(' '*sp+'*'*mi)
+    for j in range(t-2):
+        k, l = 2*j+1, w1-2*j
+        s = '*'+' '*k+'*'+' '*l
+        sp -= 1
+        print(' '*sp+(s*(i+1))[:-l-1]+'*')
+print('*'*w)
